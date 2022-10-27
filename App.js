54b4515540a45e51ac0c8c772b3cda5fe9869999
Rabah GHILES <rabahghiles@gmail.com>
2022-10-27 12:37:17 +0200

    add vscode folder to .gitignore

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hackathon</Text>
+      <Text>hackathon 3</Text>
       <StatusBar style="auto" />
     </View>
   );
